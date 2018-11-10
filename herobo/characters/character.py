554dd78300e0f880aa61cb8f46a2359ddd39562d
Tyler Lloyd <tyjoll96@gmail.com>
2018-11-10 14:54:46 -0800

    Move basic example into a character class

--- herobo/characters/character.py
+++ herobo/characters/character.py
@@ -6,3 +6,9 @@ class Character:
             'x': 0,
             'y': 0
         }
+
+    def act(self):
+        self.position['x'] = (self.position['x'] + 1) % pyxel.width
+
+    def render(self):
+        pyxel.rect(self.position['x'], 0, self.position['x'] + 7, 7, 9)
--- herobo/herobo.py
+++ herobo/herobo.py
@@ -1,16 +1,18 @@
 import pyxel
+from characters.character import Character 
 
 class App:
     def __init__(self):
         pyxel.init(160, 120)
         self.x = 0
+        self.player = Character()
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        self.x = (self.x + 1) % pyxel.width
+        self.player.act()
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.rect(self.x, 0, self.x + 7, 7, 9)
+        self.player.render()
 
 App()
